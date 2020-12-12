ba573d72c9362c3e35ed65a8f5153a7f495ff2a2
julliettee <drozdovajulia2002@gmail.com>
2020-12-12 20:18:37 +0300

    Add first people

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include "Person.h"
 
+const std::string Male = "male";
+const std::string Female = "female";
+const std::string NameEva = "Eva";
+const std::string NameAdam = "Adam";
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try{
+        Person Adam = Person(Male, NameAdam);
+        Person Eva = Person(Female, NameEva);
+        Person Kain = Person(Male, "Kain");
+        Adam.out();
+        Eva.out();
+        Kain.out();
+    }
+    catch (std::exception& exception) {
+        std::cout << exception.what() << std::endl;
+    }
+
     return 0;
 }
+
+
